e4dfadcb497e41b520974a955f666286c339682a
yarastqt <yarastqt@gmail.com>
2024-02-03 17:56:50 +0400

    feat: use button hook for button

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -1,19 +1,24 @@
 import { ReactNode, forwardRef } from 'react'
 
-import styles from './button.module.css'
 import { HoverProps, useHover } from '@react-aria/interactions'
+import { AriaButtonProps, useButton } from '@react-aria/button'
+import { useObjectRef, mergeProps } from '@react-aria/utils'
 
-export interface ButtonProps extends HoverProps {
+import styles from './button.module.css'
+
+export interface ButtonProps extends AriaButtonProps, HoverProps {
   children: ReactNode
 }
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
   const { children } = props
 
+  const rootRef = useObjectRef(ref)
   const { hoverProps } = useHover(props)
+  const { buttonProps } = useButton(props, rootRef)
 
   return (
-    <button {...hoverProps} className={styles.root} ref={ref}>
+    <button {...mergeProps(buttonProps, hoverProps)} className={styles.root} ref={rootRef}>
       {children}
     </button>
   )
