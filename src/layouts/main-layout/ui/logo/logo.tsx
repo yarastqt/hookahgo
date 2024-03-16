55d393ad61a71f1dc063dfda4795a4faeadc2d90
yarastqt <yarastqt@gmail.com>
2024-03-16 23:53:21 +0400

    feat: update logo animation

--- src/layouts/main-layout/ui/logo/logo.tsx
+++ src/layouts/main-layout/ui/logo/logo.tsx
@@ -1,31 +1,17 @@
-import { type FC, useRef } from 'react'
+import type { FC } from 'react'
 import { Link } from 'atomic-router-react'
-import gsap from 'gsap'
 
-import { useGSAP } from '@gsap/react'
-import { Logo as LogoBase } from '@app/shared/ui'
+import { FadeIn, Logo as LogoBase } from '@app/shared/ui'
 import { routes } from '@app/shared/router'
 
 import styles from './logo.module.css'
 
 export const Logo: FC = () => {
-  const ref = useRef<HTMLAnchorElement>(null)
-
-  useGSAP(
-    () => {
-      gsap.from(ref.current, {
-        delay: 0.5,
-        duration: 0.5,
-        ease: 'expoScale(0.5, 7, none)',
-        opacity: 0,
-      })
-    },
-    { scope: ref },
-  )
-
   return (
-    <Link to={routes.main} className={styles.root} ref={ref}>
-      <LogoBase />
-    </Link>
+    <FadeIn delay={0.5}>
+      <Link to={routes.main} className={styles.root}>
+        <LogoBase />
+      </Link>
+    </FadeIn>
   )
 }
