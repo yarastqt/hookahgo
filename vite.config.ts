f3c3623487c55fd7e87641b703c75839ce29f68f
yarastqt <yarastqt@gmail.com>
2024-03-31 19:00:21 +0400

    chore: setup https server

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    basicSsl({
+      name: 'hookahgo',
+      domains: ['*.hookahgo.com'],
+    }),
+  ],
 })
