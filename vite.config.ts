0f0203bffc2f273f369c938173a30411af504d30
yarastqt <yarastqt@gmail.com>
2024-04-04 04:49:49 +0400

    chore: configure generated class name for css modules

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  css: {
+    modules: {
+      generateScopedName: '[name]_[local]__[hash:base64:5]',
+    },
+  },
+
   plugins: [
     react(),
     tsconfigPaths(),
