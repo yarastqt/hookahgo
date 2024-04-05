202bf73044ced18c586d0d3ea026bb61111ce1c9
yarastqt <yarastqt@gmail.com>
2024-04-05 04:43:12 +0400

    fix: update room screen

--- src/screens/room/room-screen.tsx
+++ src/screens/room/room-screen.tsx
@@ -3,7 +3,6 @@ import { FC } from 'react'
 
 import { RoomStatus } from '@app/shared/api'
 import { Button, FadeIn, Overflow } from '@app/shared/ui'
-import { HoverEvent } from '@react-aria/interactions'
 
 import { roomScreenModel } from './model'
 import styles from './room-screen.module.css'
@@ -14,21 +13,6 @@ export const RoomScreen: FC = () => {
     onRejectPress: roomScreenModel.rejectPressed,
     room: roomScreenModel.$room,
   })
-
-  const onHoverStart = (event: HoverEvent) => {
-    const rect = event.target.getBoundingClientRect()
-
-    const canvasHeight = document.body.offsetHeight
-    const canvasWidth = document.body.offsetWidth
-
-    const randX = getRandom(0, canvasWidth)
-    const randY = getRandom(0, canvasHeight)
-
-    const x = clamp(randX - rect.width - rect.x, 0, canvasWidth)
-    const y = clamp(randY - rect.height - rect.y, 0, canvasHeight)
-
-    event.target.style.transform = `translate3d(${x}px, ${y}px, 0)`
-  }
 
   const isPending = room?.status === RoomStatus.Pending
   const isAccepted = !isPending && room?.status === RoomStatus.Accepted
@@ -43,37 +27,20 @@ export const RoomScreen: FC = () => {
           </FadeIn>
         </Overflow>
 
-        <Overflow>
-          <FadeIn className={styles.actions} delay={1.5}>
-            <Button
-              excludeFromTabOrder
-              isInactive={isRejected}
-              onPress={onAccceptPress}
-              variant="action"
-            >
+        <div className={styles.actions}>
+          <FadeIn className={styles.action} delay={2}>
+            <Button isInactive={isRejected} onPress={onAccceptPress} variant="action">
               Конечно да
             </Button>
+          </FadeIn>
 
-            <Button
-              excludeFromTabOrder
-              isInactive={isAccepted}
-              // onHoverStart={onHoverStart}
-              onPress={onRejectPress}
-              variant="danger"
-            >
+          <FadeIn className={styles.action} delay={2.5}>
+            <Button isInactive={isAccepted} onPress={onRejectPress} variant="danger">
               Нет, я не игрок
             </Button>
           </FadeIn>
-        </Overflow>
+        </div>
       </div>
     </div>
   )
-}
-
-function getRandom(min: number, max: number) {
-  return Math.random() * (max - min) + min
-}
-
-function clamp(num: number, min: number, max: number) {
-  return Math.min(Math.max(num, min), max)
 }
