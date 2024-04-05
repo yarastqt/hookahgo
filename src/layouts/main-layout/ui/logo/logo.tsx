3d23819aa5481027f0dfb868a03a6fe1e859aa4a
yarastqt <yarastqt@gmail.com>
2024-04-05 04:33:34 +0400

    feat: add focus ring for tabbable components

--- src/layouts/main-layout/ui/logo/logo.tsx
+++ src/layouts/main-layout/ui/logo/logo.tsx
@@ -1,17 +1,20 @@
-import type { FC } from 'react'
 import { Link } from 'atomic-router-react'
+import type { FC } from 'react'
 
-import { FadeIn, Logo as LogoBase } from '@app/shared/ui'
 import { routes } from '@app/shared/router'
+import { FadeIn, Logo as LogoBase } from '@app/shared/ui'
+import { FocusRing } from '@react-aria/focus'
 
 import styles from './logo.module.css'
 
 export const Logo: FC = () => {
   return (
     <FadeIn delay={0.5}>
-      <Link to={routes.main} className={styles.root}>
-        <LogoBase />
-      </Link>
+      <FocusRing focusRingClass={styles.focusRing}>
+        <Link to={routes.main} className={styles.root}>
+          <LogoBase />
+        </Link>
+      </FocusRing>
     </FadeIn>
   )
 }
--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -1,11 +1,12 @@
 import cx from 'clsx'
-import { ReactNode, forwardRef } from 'react'
 import { motion } from 'framer-motion'
+import { ReactNode, forwardRef } from 'react'
 
-import { HoverProps, useHover } from '@react-aria/interactions'
-import { AriaButtonProps, useButton } from '@react-aria/button'
-import { useObjectRef, mergeProps } from '@react-aria/utils'
 import { SupressWarning } from '@app/shared/lib/utility-types'
+import { AriaButtonProps, useButton } from '@react-aria/button'
+import { FocusRing } from '@react-aria/focus'
+import { HoverProps, useHover } from '@react-aria/interactions'
+import { mergeProps, useObjectRef } from '@react-aria/utils'
 
 import styles from './button.module.css'
 
@@ -24,29 +25,31 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) =>
   const { buttonProps, isPressed } = useButton(props, rootRef)
 
   return (
-    <motion.button
-      {...mergeProps<SupressWarning>(buttonProps, hoverProps)}
-      className={cx(
-        styles.root,
-        {
-          [styles.root_isHovered]: isHovered,
-          [styles.root_isInactive]: isInactive,
-          [styles.root_isPressed]: isPressed,
-          [styles.root_variant_action]: variant === 'action',
-          [styles.root_variant_danger]: variant === 'danger',
-        },
-        className,
-      )}
-      ref={rootRef}
-      whileTap={{ scale: 0.96 }}
-      transition={{
-        type: 'spring',
-        damping: 30,
-        duration: 0.2,
-        stiffness: 300,
-      }}
-    >
-      {children}
-    </motion.button>
+    <FocusRing focusRingClass={styles.focusRing}>
+      <motion.button
+        {...mergeProps<SupressWarning>(buttonProps, hoverProps)}
+        className={cx(
+          styles.root,
+          {
+            [styles.root_isHovered]: isHovered,
+            [styles.root_isInactive]: isInactive,
+            [styles.root_isPressed]: isPressed,
+            [styles.root_variant_action]: variant === 'action',
+            [styles.root_variant_danger]: variant === 'danger',
+          },
+          className,
+        )}
+        ref={rootRef}
+        whileTap={{ scale: 0.96 }}
+        transition={{
+          type: 'spring',
+          damping: 30,
+          duration: 0.2,
+          stiffness: 300,
+        }}
+      >
+        {children}
+      </motion.button>
+    </FocusRing>
   )
 })
