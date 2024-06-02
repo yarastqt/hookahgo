21fb9d889f2c0aa4e4101e42dc324e2368f14b1d
yarastqt <yarastqt@gmail.com>
2024-06-03 02:38:30 +0400

    feat: impl not found screen

---
+++ src/screens/not-found/not-found-screen.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react'
+
+import { routes } from '@app/shared/router'
+import { Button } from '@app/shared/ui'
+
+import styles from './not-found-screen.module.css'
+
+export const NotFoundScreen: FC = () => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.title}>Страница не найдена x_X</div>
+
+      <Button to={routes.main} variant="action">
+        Вернуться на главную
+      </Button>
+    </div>
+  )
+}
--- src/screens/screens.tsx
+++ src/screens/screens.tsx
@@ -3,10 +3,12 @@ import { createRoutesView } from 'atomic-router-react'
 import { MainLayout } from '@app/layouts/main-layout'
 
 import { MainScreen } from './main'
+import { NotFoundScreen } from './not-found'
 import { RoomScreen } from './room'
 
 const RoutesView = createRoutesView({
   routes: [MainScreen, RoomScreen],
+  otherwise: NotFoundScreen,
 })
 
 export const Screens = () => {
