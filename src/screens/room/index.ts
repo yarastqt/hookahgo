791d4057438c47e9b253292bdb6ac9219a41b609
yarastqt <yarastqt@gmail.com>
2024-06-02 04:40:37 +0400

    feat: remove skeleton for room screen

--- src/screens/room/index.ts
+++ src/screens/room/index.ts
@@ -2,15 +2,13 @@ import { createRouteView } from 'atomic-router-react'
 
 import { routes } from '@app/shared/router'
 
-import { RoomScreen as view } from './room-screen'
 import { roomScreenLoadedRoute } from './model'
-import { ScreenSkeleton } from './ui/screen-skeleton'
+import { RoomScreen as view } from './room-screen'
 
 export const RoomScreen = {
   view: createRouteView<unknown, {}, {}>({
     route: roomScreenLoadedRoute,
     view: view,
-    otherwise: ScreenSkeleton,
   }),
   route: routes.room,
 }
--- src/screens/room/ui/screen-skeleton/screen-skeleton.tsx
+++
@@ -1,5 +0,0 @@
-import { FC } from 'react'
-
-export const ScreenSkeleton: FC = () => {
-  return <div style={{ color: 'red' }}>Loading...</div>
-}
