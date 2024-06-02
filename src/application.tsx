828779f0721cb5d0019147eeac37775fa30cfe00
yarastqt <yarastqt@gmail.com>
2024-06-02 20:53:37 +0400

    refactor: use invariant for check root element

--- src/application.tsx
+++ src/application.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'atomic-router-react'
 import { allSettled } from 'effector'
 import { Provider } from 'effector-react'
 import { createRoot } from 'react-dom/client'
+import invariant from 'ts-invariant'
 
 import { Screens } from '@app/screens'
 import { appStarted, scope } from '@app/shared/config'
@@ -11,10 +12,14 @@ import { history, router } from '@app/shared/router'
 import './application/global.css'
 
 async function render() {
+  const root = document.getElementById('root')
+
+  invariant(root)
+
   allSettled(appStarted, { scope: scope })
   allSettled(router.setHistory, { scope: scope, params: history })
 
-  createRoot(document.getElementById('root')!).render(
+  createRoot(root).render(
     <Provider value={scope}>
       <RouterProvider router={router}>
         <Screens />
